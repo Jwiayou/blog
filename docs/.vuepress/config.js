11d1a9bc77cc7fa881eaf111050b5a713773ebf4
jiangwei <15168472715@163.com>
2018-07-30 09:32:18 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'WhiteDeer',
+  themeConfig: {
+    nav: [
+      {text: '笔记&文章', link: '/'},
+      {text: '日记', link: '/diary/'},
+      {text: 'sf.gg', link: 'https://segmentfault.com/u/jiangwei_5851efa237839/articles'}
+    ]
+  }
+};
+
